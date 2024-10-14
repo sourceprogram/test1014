2cfb5aa18021dd00098a626c03cf06de11f5e312
sourceprogram <2065455001@qq.com>
2024-10-14 17:52:28 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -13,7 +13,7 @@ export const APP_INFO: AppInfo = {
 export const isShowPrompt = false
 export const promptTemplate = 'I want you to act as a javascript console.'
 
-export const API_PREFIX = '/api'
+export const API_PREFIX = 'http://172.16.45.8/v1'
 
 export const LOCALE_COOKIE_NAME = 'locale'
 
